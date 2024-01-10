0579437395fea6a3d73e645e1f6b184637700dfc
kazmonroy <katherinemonroy@gmail.com>
2024-01-10 09:38:38 +0100

    Add initial files

---
+++ src/ui/AppLayout.tsx
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+function AppLayout() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default AppLayout;
---
+++ src/ui/Header.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Via Napoli</Link>
+      <p>Katherine</p>
+    </header>
+  );
+}
+
+export default Header;
